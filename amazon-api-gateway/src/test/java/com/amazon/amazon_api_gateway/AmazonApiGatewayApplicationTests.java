83ef1cae17612222a5405586e7bf3e08c16a79e7
ruchi <yalakantisairuchitha@gmail.com>
2026-08-12 12:01:39 +0530

    first commit

---
+++ amazon-api-gateway/src/test/java/com/amazon/amazon_api_gateway/AmazonApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.amazon_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
